ccf69caec98ceb484799559013a074997e001ab2
Himanshu <araj00990@gmail.com>
2022-05-02 16:20:32 +0530

    ongoing

--- Assignment-04/CNN/losses.py
+++ Assignment-04/CNN/losses.py
@@ -1,55 +1,21 @@
 import numpy as np
 
 
-def softmax(X):
-    exps = np.exp(X)
-    return exps / np.sum(exps)
+def cross_entropy(y_true, y_pred):
+    """
+    Computes the cross entropy between y_true and y_pred.
+    y_true is a one-hot encoded vector of shape (batch_size, num_classes).
+    y_pred is a vector of shape (batch_size, num_classes).
 
-
-def mse(y_true, y_pred):
-    return np.mean(np.power(y_true - y_pred, 2))
-
-
-def mse_prime(y_true, y_pred):
-    return 2 * (y_pred - y_true) / np.size(y_true)
-
-
-def binary_cross_entropy(y_true, y_pred):
-    # check if log is not going to be valid
-
+    """
     return np.mean(-y_true * np.log(y_pred) - (1 - y_true) * np.log(1 - y_pred))
 
 
-def binary_cross_entropy_prime(y_true, y_pred):
+def cross_entropy_prime(y_true, y_pred):
+    """
+    Computes the derivative of cross entropy with respect to y_pred.
+    y_true is a one-hot encoded vector of shape (batch_size, num_classes).
+    y_pred is a vector of shape (batch_size, num_classes).
+
+    """
     return ((1 - y_true) / (1 - y_pred) - y_true / y_pred) / np.size(y_true)
-
-
-def cross_entropy(y, X):
-    """
-    X is the output from fully connected layer (num_examples x num_classes)
-    y is labels (num_examples x 1)
-        Note that y is not one-hot encoded vector. 
-        It can be computed as y.argmax(axis=1) from one-hot encoded vectors of labels if required.
-    """
-    m = y.shape[0]
-    p = softmax(X)
-    # We use multidimensional array indexing to extract
-    # softmax probability of the correct label for each sample.
-    # Refer to https://docs.scipy.org/doc/numpy/user/basics.indexing.html#indexing-multi-dimensional-arrays for understanding multidimensional array indexing.
-    log_likelihood = -np.log(p[range(m), y])
-    loss = np.sum(log_likelihood) / m
-    return loss
-
-
-def cross_entropy_prime(X, y):
-    """
-    X is the output from fully connected layer (num_examples x num_classes)
-    y is labels (num_examples x 1)
-        Note that y is not one-hot encoded vector. 
-        It can be computed as y.argmax(axis=1) from one-hot encoded vectors of labels if required.
-    """
-    m = y.shape[0]
-    grad = softmax(X)
-    grad[range(m), y] -= 1
-    grad = grad/m
-    return grad
--- Assignment-04/CNN/network.py
+++ Assignment-04/CNN/network.py
@@ -1,24 +1,43 @@
 def predict(network, input):
+    """
+    :param network: list of layers
+    :param input: input data
+    :return: output of the network
+
+    """
     output = input
     for layer in network:
-        output = layer.forward(output)
+        output = layer.forward_pass(output)
     return output
 
-def train(network, loss, loss_prime, x_train, y_train, epochs = 1000, learning_rate = 0.01, verbose = True):
+
+def train(network, loss, loss_derivative, x_train, y_train, learning_rate, epochs):
+    """
+    :param network: list of layers
+    :param loss: loss function
+    :param loss_derivative: derivative of loss function
+    :param x_train: input data
+    :param y_train: output data
+    :param learning_rate: learning rate
+    :param epochs: number of epochs
+    :param verbose: print loss after each epoch
+    :return: None
+
+    """
     for e in range(epochs):
         error = 0
         for x, y in zip(x_train, y_train):
-            # forward
+            # forward_pass
             output = predict(network, x)
 
             # error
             error += loss(y, output)
 
             # backward
-            grad = loss_prime(y, output)
+            grad = loss_derivative(y, output)
             for layer in reversed(network):
                 grad = layer.backward(grad, learning_rate)
 
         error /= len(x_train)
-        if verbose:
-            print(f"{e + 1}/{epochs}, error={error}")
+
+        print(f"{e + 1}/{epochs}, ----- error for this epoch is={error}")
